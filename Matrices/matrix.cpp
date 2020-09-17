e185156672f6ba88a09a5acfcab065563d1f8628
Sycophantam <61561014+Sycophantam@users.noreply.github.com>
2020-09-17 11:49:11 -0700

    Add files via upload

--- Matrices/matrix.cpp
+++ Matrices/matrix.cpp
@@ -4,11 +4,38 @@
 #include <algorithm>
 #include <cmath>
 
+/**********************************************************
+ *
+ * Constructor Matrix: Class Matrix
+ *_________________________________________________________
+ * This method constructs the Matrix class
+ *_________________________________________________________
+ * PRE-CONDITIONS
+ *     none
+ *
+ * POST-CONDITIONS
+ *     This function will construct a Matrix class
+ ***********************************************************/
 Matrix::Matrix()
 {
     content = new double(0);
     size = n = m = 1;
 }
+
+/**********************************************************
+ *
+ * Constructor Matrix: Class Matrix
+ *_________________________________________________________
+ * This method constructs the Matrix class
+ *_________________________________________________________
+ * PRE-CONDITIONS
+ *     The following need pre-defined values:
+ *      n(int)-Number of rows in the matrix
+ *      m(int)-Number of columns in the matrix
+ *
+ * POST-CONDITIONS
+ *     This function will construct a Matrix class
+ ***********************************************************/
 Matrix::Matrix(int n,   //Number of rows in the matrix
                int m)   //Number of columns in the matrix
 {
@@ -21,6 +48,22 @@ Matrix::Matrix(int n,   //Number of rows in the matrix
         *(content + i) = 0;
     size = n*m;
 }
+
+/**********************************************************
+ *
+ * Constructor Matrix: Class Matrix
+ *_________________________________________________________
+ * This method constructs the Matrix class
+ *_________________________________________________________
+ * PRE-CONDITIONS
+ *     The following need pre-defined values:
+ *      n(int)-Number of rows in the matrix
+ *      m(int)-Number of columns in the matrix
+ *      meat(vector<double>)-Values in the matrix
+ *
+ * POST-CONDITIONS
+ *     This function will construct a Matrix class
+ ***********************************************************/
 Matrix::Matrix(int n,               //Number of rows in the matrix
                int m,               //Number of columns in the matrix
                vector<double> meat) //Values to be put in the matrix
@@ -30,7 +73,7 @@ Matrix::Matrix(int n,               //Number of rows in the matrix
     size = n*m;
 
     //Checking if the vector's content can evenly fit in the matrix
-    if(meat.size() != size)
+    if(meat.size() != static_cast<unsigned>(size))
     {
         cout << "Error: Vector of size " << meat.size() << " cannot fit in " <<
                 n << " by " << m << " matrix" << endl;
@@ -43,16 +86,103 @@ Matrix::Matrix(int n,               //Number of rows in the matrix
 
 }
 
-Matrix::Matrix(Matrix &mat)
+/**********************************************************
+ *
+ * Constructor Matrix: Class Matrix
+ *_________________________________________________________
+ * This method constructs the Matrix class
+ *_________________________________________________________
+ * PRE-CONDITIONS
+ *     The following need pre-defined values:
+ *      n(int)-Number of rows in the matrix
+ *      m(int)-Number of columns in the matrix
+ *      meat(vector<double>)-Values in the matrix
+ *      b(vector<double>)-Vector of answers
+ *
+ * POST-CONDITIONS
+ *     This function will construct a Matrix class
+ ***********************************************************/
+Matrix::Matrix(int n,                   //Number of rows in the matrix
+               int m,                   //Number of columns in the matrix
+               vector<double> &meat,    //Values to be put in the matrix
+               vector<double> &b)       //Vector of answers
+{
+    Matrix mat(n, m, meat);
+    *this = mat;
+    bValues = b;
+
+}
+
+/**********************************************************
+ *
+ * Constructor Matrix: Class Matrix
+ *_________________________________________________________
+ * This method constructs the Matrix class
+ *_________________________________________________________
+ * PRE-CONDITIONS
+ *     The following need pre-defined values:
+ *      mat(Matrix)-Matrix to copy
+ *
+ * POST-CONDITIONS
+ *     This function will construct a Matrix class
+ ***********************************************************/
+Matrix::Matrix(Matrix &mat)     //Matrix to copy
 {
     n = mat.n;
     m = mat.m;
     size = n*m;
+    if(content != nullptr)
+    {
+        delete content;
+        content = nullptr;
+    }
 
+    content = new double[size];
+
+    //Copying the values from the original matrix into the copied
+    for(int i = 0; i < mat.size; i++)
+    {
+        *(content + i) = *(mat.content + i);
+    }
+    bValues = mat.bValues;
 
 }
 
-void Matrix::print(vector<double> &b)
+/**********************************************************
+ *
+ * Constructor Matrix: Class Matrix
+ *_________________________________________________________
+ * This method constructs the Matrix class
+ *_________________________________________________________
+ * PRE-CONDITIONS
+ *     The following need pre-defined values:
+ *      mat(Matrix)-Matrix to copy
+ *      b(vector<double>)-Answer vector
+ *
+ * POST-CONDITIONS
+ *     This function will construct a Matrix class
+ ***********************************************************/
+Matrix::Matrix(Matrix &mat,         //Matrix to copy
+               vector<double> &b)   //Vector as answers
+{
+    *this = mat;
+    bValues = b;
+}
+
+/**********************************************************
+ *
+ * Method print(): Class Matrix
+ *_________________________________________________________
+ * This method prints the matrix and its values
+ *_________________________________________________________
+ * PRE-CONDITIONS
+ *     The following need pre-defined values:
+ *      None
+ *
+ * POST-CONDITIONS
+ *     This function returns nothing
+ ***********************************************************/
+void Matrix::print()
 {
     //We want to make sure there is content before we print the matrix
     assert(size != 0);
@@ -61,19 +191,36 @@ void Matrix::print(vector<double> &b)
         cout << "[";
         for(int j = 0; j < m; j++)
         {
-            cout << setw(7) << left << setprecision(2) << fixed << *(content + (i*m + j));
+            cout << setw(7) << left
+                 << setprecision(2) << fixed << *(content + (i*m + j));
         }
-        cout << setw(7) << right << setprecision(2) << fixed << b.at(i);
+        if(!bValues.empty())
+            cout << setw(7) << right
+                 << setprecision(2) << fixed << bValues.at(i);
         cout << "]" << endl;
     }
 
 }
 
-Matrix Matrix::solve(vector<double> b)  //Vector of b values
+/**********************************************************
+ *
+ * Method solve(): Class Matrix
+ *_________________________________________________________
+ * This method uses Gaussian elimination with scaled partial
+ * pivoting to solve the system of matrix
+ *_________________________________________________________
+ * PRE-CONDITIONS
+ *     The following need pre-defined values:
+ *      None
+ *
+ * POST-CONDITIONS
+ *     This function returns the modified matrix
+ ***********************************************************/
+Matrix Matrix::solve()  //Vector of b values
 {
     //Variable that is stored as the last value in the exclusion vector
 
-    print(b);
+    print();
     //Scale vector
     vector<double> scale = getScale();
     if(DEBUG)
@@ -114,13 +261,13 @@ Matrix Matrix::solve(vector<double> b)  //Vector of b values
                 cout << "Exclusion vector:";
                 printV(exc);
                 cout << "Pivot indices: ";
-                for(int afa = 0; afa < pivot.size(); afa++)
+                for(unsigned afa = 0; afa < pivot.size(); afa++)
                 {
                     cout << pivot.at(afa).index << " ";
                 }
                 cout << endl;
                 cout << "Pivot ratios: ";
-                for(int afa = 0; afa < pivot.size(); afa++)
+                for(unsigned afa = 0; afa < pivot.size(); afa++)
                 {
                     cout << pivot.at(afa).ratio << " ";
                 }
@@ -129,13 +276,13 @@ Matrix Matrix::solve(vector<double> b)  //Vector of b values
             }
         }
         cout << "Pivot ratios: ";
-        for(int afa = 0; afa < pivot.size(); afa++)
+        for(unsigned u = 0; u < pivot.size(); u++)
         {
-            cout << pivot.at(afa).ratio << " ";
+            cout << pivot.at(u).ratio << " ";
         }
         cout << endl;
         int pivotRow = getMaxIndex(pivot);
-        cout << "Pivot row is " << pivotRow << endl;
+        cout << "Pivot row is " << pivotRow + 1 << endl;
         cout << endl;
 
 
@@ -143,7 +290,6 @@ Matrix Matrix::solve(vector<double> b)  //Vector of b values
         //front so that bad is constantly at the back
         exc.insert(exc.begin(), pivotRow);
         //printV(exc);
-        int scale = 0;
         for(int k = 0; k < n; k++)
         {
             if(DEBUG)
@@ -170,35 +316,62 @@ Matrix Matrix::solve(vector<double> b)  //Vector of b values
                     cout << "k is " << k << endl;
                     cout << "pivotRow is " << pivotRow << endl;
                 }
-//                print(b);
-//                cout << endl;
-                addRow(pivotRow, k, scalar, b);
-                print(b);
+                addRow(pivotRow, k, scalar);
+                print();
                 cout << endl;
             }
         }
         pivot.clear();
         coef.clear();
     }
+    cout << "Exclusion vector" << endl;
+    printV(exc);
+    return *this;
 }
 
-double Matrix::getScalar(int cons, int change, int column)
+/**********************************************************
+ *
+ * Method getScalar(): Class Matrix
+ *_________________________________________________________
+ * This method finds the factor needed to reduce a row
+ *_________________________________________________________
+ * PRE-CONDITIONS
+ *     The following need pre-defined values:
+ *      cons(int)-The row that is not changed
+ *      change(int)-The row that is modified
+ *      column(int)-The column to reduce
+ *
+ * POST-CONDITIONS
+ *     This function returns the scalar needed to reduce the first nonzero value
+ *      in the row to zero
+ ***********************************************************/
+double Matrix::getScalar(int cons,      //Row that will not be modified
+                         int change,    //Row that will be modified
+                         int column)    //Column to base the scalar off of
 {
-    if(DEBUG)
-    {
-        cout << "Getting the scalar" << endl;
-        cout << "The numerator should be " << *(content + change*m + column);
-        cout << endl;
-        cout << "The denominator should be " << *(content + cons*m + column);
-        cout << endl;
-    }
     return -1 * ((*(content + change*m + column))/
                  (*(content + cons*m + column)));
 }
+
+/**********************************************************
+ *
+ * Method addRow(): Class Matrix
+ *_________________________________________________________
+ * This method adds row cons to row change
+ *_________________________________________________________
+ * PRE-CONDITIONS
+ *     The following need pre-defined values:
+ *      cons(int)-The row that is adding
+ *      change(int)-The row that is changed
+ *      scalar(double)-The factor needed to reduce one term to zero when adding
+ *                      cons to change
+ *
+ * POST-CONDITIONS
+ *     This function returns nothing
+ ***********************************************************/
 void Matrix::addRow(int cons,           //Row that is adding to change
                     int change,         //Row that is being added to
-                    double scalar,      //Scale factor of the row
-                    vector<double> &b)  //Answer vector
+                    double scalar)      //Scale factor of the row
 {
     for(int i = 0; i < m; i++)
     {
@@ -206,11 +379,25 @@ void Matrix::addRow(int cons,           //Row that is adding to change
         //Cons*m + i gives the index that is adding to change
         *(content + change*m + i) += scalar * (*(content + cons*m + i));
     }
-    b.at(change) += scalar * b.at(cons);
+    if(!bValues.empty())
+        bValues.at(change) += scalar * bValues.at(cons);
 }
 
-//Finds the index of the maximum value of the given vector
-int getMaxIndex(vector<zpair> v)
+/**********************************************************
+ *
+ * Method getMaxIndex(): Class Matrix
+ *_________________________________________________________
+ * This method finds the index of the maximum ratio of coefficient to scale
+ *_________________________________________________________
+ * PRE-CONDITIONS
+ *     The following need pre-defined values:
+ *      v(zpair)-Vector of ratios and indices where they come from
+ *
+ * POST-CONDITIONS
+ *     This function returns the index of the largest ratio
+ ***********************************************************/
+int getMaxIndex(vector<zpair> v)    //Vector containing the ratios and the
+                                    //index where they came from
 {
     double max = -1000000;
     unsigned index = 0;
@@ -224,6 +411,20 @@ int getMaxIndex(vector<zpair> v)
     }
     return static_cast<int>(v.at(index).index);
 }
+
+/**********************************************************
+ *
+ * Method getScale(): Class Matrix
+ *_________________________________________________________
+ * This method finds the scale vector
+ *_________________________________________________________
+ * PRE-CONDITIONS
+ *     The following need pre-defined values:
+ *      None
+ *
+ * POST-CONDITIONS
+ *     This function returns the scale vector
+ ***********************************************************/
 vector<double> Matrix::getScale()
 {
     vector<double> scale;
@@ -234,7 +435,23 @@ vector<double> Matrix::getScale()
     return scale;
 }
 
-double Matrix::find_max(int start, int end)
+/**********************************************************
+ *
+ * Method find_max(): Class Matrix
+ *_________________________________________________________
+ * This method finds the maximum value in each row of the matrix
+ *_________________________________________________________
+ * PRE-CONDITIONS
+ *     The following need pre-defined values:
+ *      start(int)-Starting location to look for
+ *      end(int)-Ending location of the list
+ *
+ * POST-CONDITIONS
+ *     This function returns the maximum value between content[start]
+ *      and content[end]
+ ***********************************************************/
+double Matrix::find_max(int start,  //Index of content to start looking
+                        int end)    //Index of content to stop looking
 {
     double max = -100000;
     for(int i = start; i < end; i++)
@@ -245,20 +462,37 @@ double Matrix::find_max(int start, int end)
     return abs(max);
 }
 
+/**********************************************************
+ *
+ * Operator =: Class Matrix
+ *_________________________________________________________
+ * Overloaded assignment operator
+ *_________________________________________________________
+ * PRE-CONDITIONS
+ *     The following need pre-defined values:
+ *      other(Matrix)-Matrix to copy
+ *
+ * POST-CONDITIONS
+ *     This function returns a copy of other
+ ***********************************************************/
 Matrix Matrix::operator=(Matrix &other)
 {
-    Matrix m;
-    m.n = other.n;
-    m.m = other.m;
-    m.size = m.n*m.m;
+    n = other.n;
+    m = other.m;
+    size = n*m;
+    bValues = other.bValues;
 
-    //Checking if the vector's content can evenly fit in the matrix
-    m.content = new double[size];
-    std::copy(other.getVec().begin(), other.getVec().end(), m.content);
-    return m;
+    if(content != nullptr)
+    {
+        delete content;
+        content = nullptr;
+    }
+
+    content = new double[size];
+    for(int i = 0; i < other.size; i++)
+    {
+        *(content + i) = *(other.content + i);
+    }
+    return *this;
 }
 
-vector<double> Matrix::getVec()
-{
-    return vector<double> (content, content + size);
-}
--- Matrices/matrix.h
+++ Matrices/matrix.h
@@ -8,6 +8,7 @@ using namespace std;
 
 const bool DEBUG = false;
 
+//Quick way to store a coefficient and its ratio to the scale value
 struct zpair{
     double ratio = 0;
     int index = 0;
@@ -17,7 +18,11 @@ struct zpair{
         index = i;
     }
 };
+
+
 int getMaxIndex(vector<zpair> v);
+
+
 template<class T>
 void printV(vector<T> v)
 {
@@ -25,30 +30,52 @@ void printV(vector<T> v)
         cout << v.at(u) << " ";
     cout << endl;
 }
+
+//Class that represents a Matrix
 class Matrix
 {
 
 
 public:
+    /******************
+     ** CONSTRUCTORS **
+     ******************/
     Matrix();
     Matrix(int n, int m);
     Matrix(int n, int m, vector<double> meat);
+    Matrix(int n, int m, vector<double> &meat, vector<double> &b);
     Matrix(Matrix &mat);
-    void print(vector<double> &b);
-    Matrix solve(vector<double> b);
-    void addRow(int cons, int change, double scalar, vector<double> &b);
+    Matrix(Matrix &mat, vector<double> &b);
+
+    /***************
+     ** ACCESSORS **
+     ***************/
+    void print();
+    Matrix solve();
+
     Matrix operator=(Matrix &other);
-    vector<double> getVec();
 
 private:
 
-    int n = 0;
-    int m = 0;
-    int size = 0;
-    double* content = nullptr;
+    int n = 0;                  //Number of rows
+    int m = 0;                  //Number of columns
+    int size = 0;               //m*n. Size of 1D array
+    double* content = nullptr;  //Dynamically allocated 1D array that stores all
+                                //values in the matrix
+    vector<double> bValues;     //Answer vector
+
+    /***************
+     ** ACCESSORS **
+     ***************/
     vector<double> getScale();
-    double getScalar(int cons, int change, int column);
     double find_max(int start, int end);
+
+    /***************
+     ** MUTATORS  **
+     ***************/
+    void addRow(int cons, int change, double scalar);
+    double getScalar(int cons, int change, int column);
+
 
 
 
